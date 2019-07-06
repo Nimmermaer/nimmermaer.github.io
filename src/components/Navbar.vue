6c73fefa4ffd493a852cc8ba58d05152c6bdaa5e
Michael Blunck <michidrau@gmail.com>
2019-07-06 23:09:24 +0200

    [TASK] Adds routing chapter

---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li>
+        <router-link :to="{name: 'Home'}">Home</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'About'}">About</router-link>
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{name:'ViewProfile', params: {user_id: id}}"><span>Profile {{id}}</span></router-link>
+      </li>
+    </ul>
+    <h2>nNavigation Controls</h2>
+    <ul>
+      <li>
+        <button @click="goBack">Go Back</button></li>
+      <li>
+        <button @click="goHome">Redirect to Home</button></li>
+      <li>
+        <button @click="goForward">Forward</button></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      userIds: ['1', '2', '3', '4']
+    }
+  },
+  methods: {
+    goHome () {
+      this.$router.push({name: 'Home'})
+    },
+    goBack () {
+      this.$router.go(-1)
+    },
+    goForward () {
+      this.$router.go(+1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  a {
+    color: blue;
+  }
+
+  a.router-link-exact-active {
+    color: purple;
+  }
+</style>
